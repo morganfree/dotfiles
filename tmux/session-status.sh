2f2c3517ddfb0b0c08a1663dca8633b41285b94d
morganfree <sebastiandomas@gmail.com>
2018-12-02 01:35:29 +0100

    hide session status if there is only 1 session, update tmux config

--- tmux/session-status.sh
+++ tmux/session-status.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 status=""
+sessions=$(tmux ls -F '#S')
 
-for session in $(tmux ls -F '#S'); do
+if [ "$( echo "$sessions" | wc -l )" -le 1 ]; then exit; fi
+
+for session in $sessions; do
   if [ "$session" = "$1" ]; then
     status="$status#[fg=green]$session #[default]"
   else
@@ -10,4 +13,4 @@ for session in $(tmux ls -F '#S'); do
   fi
 done
 
-echo "$status"
+echo "$status#[fg=white,bold]| "
